e879de1c3bfbb0e632f9c99bfbe14d8f3984e9e4
Symion Edwards <symion.edwards@gmail.com>
2020-06-24 09:15:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0
+    puts "Fizz"
+  elseif int % 5 == 0
+    puts "Buzz"
+  else int % 3 == 0 && % 5 == 0
+    puts "Fizzbuzz"
 end
